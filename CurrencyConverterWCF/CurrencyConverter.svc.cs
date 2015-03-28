c70dcec68622eb5cbc7f4e6751a1af65fcedb9b5
Ygor Reis <ygorreis@hotmail.com>
2015-03-28 19:10:34 -0300

    First Commit

---
+++ CurrencyConverterWCF/CurrencyConverter.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CurrencyConverterWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CurrencyConverter" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CurrencyConverter.svc or CurrencyConverter.svc.cs at the Solution Explorer and start debugging.
+    public class CurrencyConverter : ICurrencyConverter
+    {
+
+        public string getValueOfConvertedCurrency(string valCurrencyOne, string valCurrencyTwo, string amountToConvert)
+        {
+            Double valCurrencyOneDouble = 0, valCurrencyTwoDouble = 0, amountToConvertDouble = 0, showValue = 0;
+            String valueConverted = "Value Not Available";
+
+            if (!String.IsNullOrEmpty(valCurrencyOne) && !String.IsNullOrEmpty(valCurrencyTwo) && !String.IsNullOrEmpty(amountToConvert))
+            {
+
+                try
+                {
+                    valCurrencyOneDouble = Convert.ToDouble(valCurrencyOne);
+                    valCurrencyTwoDouble = Convert.ToDouble(valCurrencyTwo);
+                    amountToConvertDouble = Convert.ToDouble(amountToConvert);
+                }
+                catch (Exception e)
+                {
+                    valueConverted = "Some value is alphabetic characters. Please, use only numbers.";
+                }
+                showValue = Math.Round(((amountToConvertDouble * valCurrencyOneDouble) / valCurrencyTwoDouble),2);
+                valueConverted = Convert.ToString(showValue);
+            }
+            else
+            {
+                valueConverted = "Some value is empty. Please, fix it!";
+            }
+            return valueConverted;
+        }
+    }
+}
---
+++ CurrencyConverterWCF/ICurrencyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CurrencyConverterWCF
+{
+    [ServiceContract]
+    interface ICurrencyConverter
+    {
+        [OperationContract]
+        //[WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "getValueOfConvertedCurrency/?valCurrencyOne={valCurrencyOne}&valCurrencyTwo={valCurrencyTwo}&amountToConvert={amountToConvert}")]
+        [WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "getValueOfConvertedCurrency/{valCurrencyOne}/{valCurrencyTwo}/{amountToConvert}")]
+        //[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "convert/valCurrencyOne/valCurrencyTwo/amountToConvert")]
+        string getValueOfConvertedCurrency(string valCurrencyOne, string valCurrencyTwo, string amountToConvert);
+    }
+}
